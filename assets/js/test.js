ec714ac6cc2313f3437a4835e3e8535a9a4c07b9
chayanin <chayanin.ele@gmail.com>
2018-03-28 21:36:14 +0700

    new project4 commit

--- assets/js/test.js
+++ assets/js/test.js
@@ -2,56 +2,39 @@
 function update_member_profit(x) {
     $("#member_profit").text(String(x));
 }
+
+function set_pie_chart(x,y){
+    tmp = "c100 p"+String(parseInt(y/3))+" big";
+    $("#"+x).attr("class",tmp);  
+    $("#"+x+"_text").text(String(parseInt(y/3))+"%");  
+    console.log("change data");
+}
 // function changedata(x) {
 //     document.getElementById("solar").setAttribute("data-goal",String(x))
 // }
 
-function update_orders(y) {
-    $("#orders").text(String(y));
-}
-function update_issues(a) {
-    $("#issues").text(String(a));
-}
-function update_IPO(b) {
-    $("#IPO").text(String(b));
-}
-function update_payments(c) {
-    $("#payments").text(String(c));
-}
-function update_logistics(d) {
-    $("#logistics").text(String(d));
-}
-function update_orders2(e) {
-    $("#orders2").text(String(e));
-}
-function update_Transactions(f) {
-    $("#Transactions").text(String(f));
-}
-function update_revenue(g) {
-    $("#revenue").text(String(g));
-}
+
 // document.getElementById("solar").setAttribute("data-goal","20");
 
 $( document ).ready(function() {
     console.log("starting document!!!!");
     // Initialize Firebase
-    console.log("Initialize Firebase");
-    $("#sssss").attr("data-goal","20");
-    $("#s_solar").attr("value","20");
-    $("#s_data").text("20%");
-    console.log($("#sssss").attr("aria-valuenow"));
-    $("#sss").text("data-goal");
+    // console.log("Initialize Firebase");
+
+
     var config = {
-    apiKey: "AIzaSyCYoatdk7_r48sY_ztPK6KRojWT6hKXA8I",
-    authDomain: "my-project-4bc9b.firebaseapp.com",
-    databaseURL: "https://my-project-4bc9b.firebaseio.com",
-    projectId: "my-project-4bc9b",
-    storageBucket: "my-project-4bc9b.appspot.com",
-    messagingSenderId: "942408563134"
-  };
+        apiKey: "AIzaSyDGQ36ZE2nTGHHHsd9kT0xlg2bFLsjvm4Q",
+        authDomain: "python-training-6b3d2.firebaseapp.com",
+        databaseURL: "https://python-training-6b3d2.firebaseio.com",
+        projectId: "python-training-6b3d2",
+        storageBucket: "python-training-6b3d2.appspot.com",
+        messagingSenderId: "945887583845"
+    };
+
     firebase.initializeApp(config);
 
-    // var ref = firebase.database().ref();
+
+    var ref = firebase.database().ref();
     //
     // ref.on("value", function(snapshot) {
     //     console.log(snapshot.val().test);
@@ -62,32 +45,14 @@ $( document ).ready(function() {
 
     // total_load_activePower = 0;
 
-    var member_profitRef = firebase.database().ref("member");
-    // changedata(30)
-    member_profitRef.on("child_changed", function(data) {
+    var renew_Ref = firebase.database().ref("renewable");
+
+    renew_Ref.on("child_changed", function(data) {
         console.log(data.key);
         console.log(data.val());
-        if (data.key == "member_profit") {
-            update_member_profit(parseInt(data.val()));
-          }  else if (data.key == "orders") {
-                update_orders(parseInt(data.val()));
-          }  else if (data.key == "issues") {
-                update_issues(parseInt(data.val()));
-          }  else if (data.key == "IPO") {
-                update_IPO(parseInt(data.val()));
-          }  else if (data.key == "payments") {
-                update_payments(parseInt(data.val()));
-          }  else if (data.key == "logistics") {
-                update_logistics(parseInt(data.val()));
-          }  else if (data.key == "orders2") {
-                update_orders2(parseInt(data.val()));
-          }  else if (data.key == "Transactions") {
-                update_Transactions(parseInt(data.val()));
-          }  else if (data.key == "revenue") {
-                update_revenue(parseInt(data.val()));
-
-        } else {
-            console.log("need to parse this key " + data.key)
+        if (data.key == "solar"){
+            set_pie_chart("pie_solar",data.val())
+            // $("#pie_solar").text(String(parseInt(data.val())));
         }
     });
 
